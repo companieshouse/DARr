cd93ad669b202bcf155fb29a1a7cf2edf29a7435
Robert Eva <reva@companieshouse.gov.uk>
2022-02-18 08:54:24 +0000

    data profiling R file

--- R/data_profile.R
+++ R/data_profile.R
@@ -6,18 +6,31 @@ library(readr)
 # read csv file
 df <- 
   read_delim(
-    file = "/data/cardio_train.csv",
-    col_types = "iifidiiffffff",
-    delim=";")
+    file = "../data/companies.csv")
 
 
 # pre-processing
-df <- 
+# df <- 
   # remove the id
-  select(df, -id) %>%
+  # select(df, -id) %>%
   # age: days -> years
-  mutate(age = round(age / 365))
+  # mutate(age = round(age / 365))
 
 
 # observe first rows
 head(df)
+summary(df)
+
+# install.packages("DataExplorer")
+library(DataExplorer)
+
+
+df %>%
+  create_report(
+    output_format = html_document(toc = TRUE, toc_depth = 6, theme = "yeti"),
+    output_file = "report.html",
+    output_dir = getwd(),
+    y = NULL,
+    config = configure_report(),
+    report_title = "Data Profiling Report"
+  )
