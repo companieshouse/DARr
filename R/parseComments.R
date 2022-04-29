fe62fceb40b7e48c86c9f0481fae9dd1ad349584
Robert Eva <reva@companieshouse.gov.uk>
2022-04-29 14:56:59 +0100

    sync changes

--- R/parseComments.R
+++ R/parseComments.R
@@ -19,16 +19,48 @@ connect_remote <- Sys.getenv("db_remote")
 connect_local <- Sys.getenv("db_local")
 
 comments_dest = mongo(collection="comments", db="companies", url=connect_local)
-#You can verify that your code is now connected to the MongoDB collection by checking the total number of documents in this database. To do so, use the count() method.
+# You can verify that your code is now connected to the MongoDB collection by 
+# checking the total number of documents in this database. 
+# To do so, use the count() method.
 
 comments_dest$count()
 
 # This is the record to use as the $gte in the remote
 lr <- comments_dest$find(sort = '{"created_at": -1}', limit = 1)
 lr
+date_bst <- lr$created_at -60*60
+date_bst
+
+dte <- strftime(date_bst , "%Y-%m-%dT%H:%M:%S")
+dte
+dte <- paste(dte, "Z", sep="")
+dte
+typeof(dte)
+
 
 # Rest of code here ---
 # Get connection to the remote mongoDB (connect_remote)
+
+comments_source = mongo(collection="customer_feedback", db="customer_feedback", 
+    url=connect_remote)
+
+comments_source$count() # works with VPN on
+# Get all comments with a $gte lr
+q <- paste0('{"created_at": { "$gt" : { "$date" : "', dte, '" }}}')
+q
+lrs <- comments_source$find(
+  query = q,
+  fields = '{"_id": false }'
+)
+
+lrs[1,] # This should be the last entry in the local batch
+nrow(lrs)
+lrs <- lrs[-c(1), ]
+
+# lrs$created_at <- lr$created_at -60*60
+lrs
+feedback <- do.call("data.frame", lrs)
+feedback
 # Find the last entry date in the local collection
 # Use that as a $gte for the remote collection
 # Retrieve those records
@@ -36,8 +68,8 @@ lr
 # Write those records to the local DB
 
 # Get the dataframe from the download directory
-setwd('../data/feedback')
-feedback = read.csv('customer_feedback.csv', header = TRUE)
+# setwd('../data/feedback')
+# feedback = read.csv('customer_feedback.csv', header = TRUE)
 glimpse(feedback)
 
 # Split the URL into additional columns (data.source_url)
@@ -60,5 +92,21 @@ df_final <- df2[, c("created_at",
 # Append the final column - company number
 df_final$company_number = df2$company_split[,2]
 
+glimpse(df_final)
+
+df_out <- df_final %>% 
+  rename(
+    customer_email = data.customer_email,
+    customer_feedback = data.customer_feedback,
+    customer_name = data.customer_name
+  )
+
+glimpse(df_out)
+## # A tibble: 150 x 5
+
 # Write the finished dataset back to the working directory
-write.csv(df_final, "comments.csv", row.names = FALSE)
+# write.csv(df_final, "comments.csv", row.names = FALSE)
+
+# comments_dest = mongo(collection="comments_backup", db="companies", url=connect_local)
+comments_dest$insert(df_out)
+
